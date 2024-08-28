9261bf886ed7d3c7eef5cbbfdd96cfa4c8d8bec1
Sislax <donatello99.dm@gmail.com>
2024-08-28 16:53:08 +0200

    Operate Works

--- MangaScraper/Interfaces/IMangaScraperService.cs
+++ MangaScraper/Interfaces/IMangaScraperService.cs
@@ -1,8 +1,4 @@
-﻿using MangaScraper.Models.Domain;
-using MangaScraper.Models.Settings;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-
+﻿
 namespace MangaScraper.Interfaces
 {
     public interface IMangaScraperService
